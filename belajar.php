e40fac2060c89be9c5988ad32323287b94373390
yudhakr <yudhadeux@gmail.com>
2026-09-17 21:20:23 +0800

    belajar coba #1

---
+++ belajar.php
@@ -0,0 +1,7 @@
+<?php
+$nama = "Asep";
+$umur = 20;
+
+echo"Hello Dunia $nama";
+echo $umur*3;
+?>
---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+function jumlah(){
+    $angka1 = 20;
+    $angka2 = 13;
+    echo "penjumlahan";
+
+    echo $angka1 + $angka2;
+}
+jumlah();
+?>
