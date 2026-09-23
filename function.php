8921d2bd3945b3a815288b2ecb85558202a4d9cd
yudhakr <yudhadeux@gmail.com>
2026-09-23 22:15:18 +0800

    function #1

--- function.php
+++ function.php
@@ -2,9 +2,17 @@
 function jumlah(){
     $angka1 = 20;
     $angka2 = 13;
-    echo "penjumlahan";
+    echo "penjumlahan ";
 
     echo $angka1 + $angka2;
 }
 jumlah();
+
+function kurang(){
+    $a = 20;
+    $d = 3;
+    echo "pengurangan ";
+    echo $a - $d;
+}
+kurang();
 ?>
