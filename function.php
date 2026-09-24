1ea1f90b04733837b3f5fda08e27cb19568e0152
yudhakr <yudhadeux@gmail.com>
2026-09-24 14:35:54 +0800

    #2

--- function.php
+++ function.php
@@ -1,18 +1,36 @@
 <?php
-function jumlah(){
-    $angka1 = 20;
-    $angka2 = 13;
-    echo "penjumlahan ";
+// function jumlah(){
+//     $angka1 = 20;
+//     $angka2 = 13;
+//     echo "penjumlahan ";
 
-    echo $angka1 + $angka2;
-}
-jumlah();
+//     echo $angka1 + $angka2;
+// }
+// jumlah();
 
-function kurang(){
-    $a = 20;
-    $d = 3;
-    echo "pengurangan ";
-    echo $a - $d;
-}
-kurang();
+// function kurang(){
+//     $a = 20.5;
+//     $d = 3.2;
+//     echo "pengurangan ";
+//     echo $a - $d;
+// }
+// kurang();
+
+// $buah =[
+//     "apel",
+//     "jeruk",
+//     "mangga",
+//     "pisang", 200, 1000, 2.5, 3.5
+// ];
+// echo $buah[2];
+// echo $buah[6];
+
+$mahasiswa = [
+    "nama" => "Aditya",
+    "umur" => 24,
+    "Hobi" => "Main game",
+    "Warna kesukaan" => "Biru"
+];
+
+echo $mahasiswa["Hobi"];
 ?>
